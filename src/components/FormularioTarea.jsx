7b3b3b986814d35116b10544a6774bcfbfeb5ebd
Emilse Arias <emilsearias@gmail.com>
2023-05-23 18:37:59 -0300

    agregamos el state tarea y el array de tareas, a medida que vamos cargando tareas se carga en el array

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,16 +1,31 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+ const handleSubmit = (e)=>{
+    e.preventDefault();
+    setTareas([...tareas, tarea])
+ }
+
+
   return (
     <>
-    <Form>
-      <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="ingrese una tarea" />
-        <Button variant="primary">Agregar</Button>
-      </Form.Group>
-    </Form>
-    <ListaTareas></ListaTareas>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          <Form.Control
+            type="text"
+            placeholder="ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="primary" type="submit">Agregar</Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas></ListaTareas>
     </>
   );
 };
