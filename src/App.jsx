5a9585c9b783910d7d4a27bb54a342b1433515eb
Emilse Arias <emilsearias@gmail.com>
2023-05-23 18:04:45 -0300

    agregamos el maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Container } from 'react-bootstrap';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FormularioTarea from './components/FormularioTarea';
 
 function App() {
 
   return (
     <>
-    
+    <Container className='my-5 main'>
+    <h1 className='display-4 text-center'>Lista de tareas</h1>
+    <hr/>
+    <FormularioTarea></FormularioTarea>
+    </Container>
+    <footer className='text-center bg-dark text-light py-4'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
